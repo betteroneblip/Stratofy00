36324f136034563809e4100a1d18a02e4a5ca82c
betteroneblip <betterone857@gmail.com>
2025-12-25 23:25:43 -0300

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const audio = document.getElementById("audio");
-const nowPlaying = document.getElementById("nowPlaying");
-
-function playMusic(url, name) {
-  audio.src = url;
-  audio.play();
-  nowPlaying.innerText = "Tocando: " + name;
+function openPlaylist(nome) {
+  alert("Abrindo playlist: " + nome);
 }
