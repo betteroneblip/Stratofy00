63ebcf8b56f4b61f86e661889ade386f72d959b3
betteroneblip <betterone857@gmail.com>
2025-12-25 23:44:38 -0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function openPlaylist(nome) {
   document.getElementById("home").style.display = "none";
   document.getElementById("playlist-screen").classList.remove("hidden");
-
   document.getElementById("playlist-title").innerText = nome;
 }
 
@@ -9,4 +8,3 @@ function goHome() {
   document.getElementById("playlist-screen").classList.add("hidden");
   document.getElementById("home").style.display = "block";
 }
-
